92ab061f5dd26b5c8b0ef458cd81177de545a5e8
HannachiHassen <hannachihassen07@gmail.com>
2022-08-19 14:28:58 -0400

    Update pom.xml with the two log4j2 dependency need to generate the logs

--- src/test/java/com/TestCases/FourTest.java
+++ src/test/java/com/TestCases/FourTest.java
@@ -1,5 +1,7 @@
 package com.TestCases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -10,21 +12,29 @@ import com.Base.BaseClass;
 
 public class FourTest extends BaseClass{
 public WebDriver driver;
+Logger log;
 	
 	@BeforeMethod
 	public void setUP() {
+		log = LogManager.getLogger(FourTest.class.getName());
+		
 		driver = initializeBrowser();
+		log.debug("Browser got launched");
 		driver.get(prop.getProperty("url"));
+		log.debug("Navigated to application URL");
 	}
 	
 	@Test
 	public void testFour() throws InterruptedException {
 		System.out.println("TestFour");
+		log.info("testFour Method is started");
 	
 		driver.get("https://classic.crmpro.com/index.html");
+		log.debug("Browser got launched");
 		
 		Thread.sleep(2000);
 		Assert.assertTrue(false);
+		log.error("Login Test got failed");
 	}
 	
 	@AfterMethod
