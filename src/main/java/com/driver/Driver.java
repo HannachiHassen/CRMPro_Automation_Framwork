cd890df099edabb01a8bd8a7e4058dd405184d19
HannachiHassen <hannachihassen07@gmail.com>
2022-08-31 11:22:59 -0400

    Update 'Driver' class with initDriver, quitDriver methods

--- src/main/java/com/driver/Driver.java
+++ src/main/java/com/driver/Driver.java
@@ -1,8 +1,27 @@
 package com.driver;
 
-public class Driver {
+import java.util.Objects;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public final class Driver {
 
 	private Driver() {
+	}
+	
+	public static void initDriver() {
+		if (Objects.isNull(DriverManager.getDriver())) {
+			System.setProperty("", "");
+		}
+		DriverManager.setDriver(new FirefoxDriver());
+		DriverManager.getDriver().get(null);
+	}
+	
+	public static void quitDriver() {
+		if(Objects.nonNull(DriverManager.getDriver())) {
+			DriverManager.getDriver().quit();
+			DriverManager.unload();
+		}
 	}
 
 }
