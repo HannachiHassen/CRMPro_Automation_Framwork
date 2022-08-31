5a9a0e4bd62ebbcc96a6ecad9255f30c2ad292d0
HannachiHassen <hannachihassen07@gmail.com>
2022-08-31 14:01:27 -0400

    Create 'PropertyUtils' class within 'utils' package

--- src/main/java/com/constants/FrameworkConstants.java
+++ src/main/java/com/constants/FrameworkConstants.java
@@ -9,8 +9,8 @@ public final class FrameworkConstants {
 	private static final int PAGELOADTIMEOUT=60;
 	private static final int IMPLICITWAIT=30;    
 	
-	private static final String RESOURCESPATH= System.getProperty("user.dir") +"/CRM_Automation_Framework/resources/";
-    
+	private static final String RESOURCESPATH= System.getProperty("user.dir") +"/CRM_Automation_Framework/";
+	private static final String CONFIGFILEPATH= RESOURCESPATH + "/src/main/java/com/Config/config.properties";
 	private static final String EXCELFILEPATH = RESOURCESPATH + "/testdata/HubSpotTestData.xlsx";
 	
 	public static final int SHORT_WAIT = 3000;
@@ -23,6 +23,10 @@ public final class FrameworkConstants {
 
 	public static long getImplicitWait() {
 		return IMPLICITWAIT;
+	}
+	
+	public static String getConfigfilepath() {
+		return CONFIGFILEPATH;
 	}
 
 	public static String getTestdatasheetpath() {
--- src/main/java/com/utils/PropertyUtils.java
+++ src/main/java/com/utils/PropertyUtils.java
@@ -1,5 +1,34 @@
 package com.utils;
 
-public class PropertyUtils {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import com.constants.FrameworkConstants;
+
+public final class PropertyUtils {
+
+	private PropertyUtils() {
+		
+	}
+	
+	private static Properties property= new Properties();
+	private static final Map<String, String> CONFIGMAP =new HashMap<>();
+	
+	static {
+		try {
+			FileInputStream file = new FileInputStream(FrameworkConstants.getConfigfilepath());
+			property.load(file);
+			for (Map.Entry<Object, Object> entry : property.entrySet()) {
+				CONFIGMAP.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()).trim()); //remove the trailing and leading spaces		
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		
+		}
 }
