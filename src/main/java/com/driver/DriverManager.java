119476e424a61d088a536d7d070f660897ec8396
HannachiHassen <hannachihassen07@gmail.com>
2022-08-31 11:15:16 -0400

    Update 'DriverManager' class with TheradLocal WebDriver and the related methods

--- src/main/java/com/driver/DriverManager.java
+++ src/main/java/com/driver/DriverManager.java
@@ -8,6 +8,17 @@ public final class DriverManager {
 	
 	}
 	
-	private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
-
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	
+	public static WebDriver getDriver() {
+		return driver.get();		
+	}
+	 
+	public static void setDriver(WebDriver driverref) {
+		driver.set(driverref);
+	}
+	
+	public static void unload() {
+		driver.remove();
+	}
 }
